5077ee7693694d82a8f0222caccf95a30ad14d2b
Baráth Barnabás <barnibarath@gmail.com>
2024-02-09 12:40:00 +0100

    create controller and routing for profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index']);
+
+Route::get('/profile', [ProfileController::class, 'index']);
+
