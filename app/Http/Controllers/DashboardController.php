c90e7391b4eee022fee9c260b8add38efc001b71
Baráth Barnabás <barnibarath@gmail.com>
2024-02-15 13:28:46 +0100

    create db named youtube, create ideas migration and Idea model. Testing with displaying stored data on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Idea;
 
 class DashboardController extends Controller
 {
     public function index(){
 
-        return view('dashboard');
+        $idea = new Idea([
+            'content' => 'test2',
+        ]);
+        $idea->save();
+
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at', 'DESC')->get()
+        ]);
     }
 }
